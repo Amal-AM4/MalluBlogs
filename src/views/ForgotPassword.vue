dd64300446998b30ff398877000413fc2c4cb9f3
Amal A M <vectorhero007@gmail.com>
2025-02-27 19:56:20 +0530

    forgotpwd scss completed

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,13 +1,46 @@
 <template>
-  <div>
-    <h2>forgot</h2>
+  <div class="reset-password">
+    <div class="form-wrap">
+      <form class="reset">
+        <h2>Reset Password</h2>
+        <p>Forgot your password? Enter your email to reset it</p>
+        <div class="inputs">
+          <div class="input">
+            <input type="text" placeholder="Email" v-model="email">
+            <mail class="icon" />
+          </div>
+        </div>
+        <button>Reset</button>
+        <div class="angle"></div>
+      </form>
+      <div class="background"></div>
+    </div>
   </div>
 </template>
 
 <script setup>
+  import { ref } from "vue";
+  import mail from "../assets/Icons/envelope-regular.svg"
+
+  const email = ref('')
 
 </script>
 
 <style lang="scss">
+  .reset-password {
+    position: relative;
 
+    .form-wrap {
+      .reset {
+        h2 {
+          margin-bottom: 8px;
+        }
+
+        p {
+          text-align: center;
+          margin-bottom: 32px;
+        }
+      }
+    }
+  }
 </style>
